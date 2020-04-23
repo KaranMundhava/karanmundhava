871a2b3ac058a3c59ba2319ef087ba26cc7dd344
KaranMundhava <45780095+KaranMundhava@users.noreply.github.com>
2020-04-23 16:33:08 +0530

    some commit message

--- home/views.py
+++ home/views.py
@@ -9,6 +9,9 @@ from langdetect import detect
 def home(request):
     return render(request,'home.html')
 
+def Charts(request):
+    return render(request,'result1.html')   
+
 def getLang(langList):
     dict = {}
     
@@ -56,16 +59,121 @@ def getLang(Analysis):
     plt.title('Languages Analysis')
     plt.savefig(r"static\LangAnaly.png") 
     plt.clf()
-    return 
-     
+    return
 
-    
+
+def getVarified(Analysis):
+    name2 = ['Verified', 'Unverified','Data Unavailable']
+    count2 = [0, 0, 0]
+    for i in Analysis:
+        if i == 'true':
+            count2[0] += 1
+        elif i == 'false':
+            count2[1] += 1
+        else :
+            count2[2] +=1
+
+
+    plt.bar(name2, count2)
+    # plt.bar(name, count, align='center', alpha=0.5)
+    plt.ylabel('Count')
+    plt.title('Account Analysis')
+    plt.savefig(r"static\AccountAnaly.png")
+    plt.clf()
+    return
+
+def getCouontFollower(Analysis):
+    name2 = ['1-100', '101-200', '201-300', '301-400', '401-500', '501-600', '600+']
+    count2 = [0, 0, 0, 0, 0, 0, 0]
+    for i in Analysis:
+        if i >= 0 and i <= 100:
+            count2[0] += 1
+        elif i >= 101 and i <= 200:
+            count2[1] += 1
+        elif i >= 201 and i <= 300:
+            count2[2] += 1
+        elif i >= 301 and i <= 400:
+            count2[3] += 1
+        elif i >= 401 and i <= 500:
+            count2[4] += 1
+        elif i >= 501 and i <= 600:
+            count2[5] += 1
+        else:
+            count2[6] += 1
+    plt.bar(name2, count2)
+    # plt.bar(name, count, align='center', alpha=0.5)
+    plt.ylabel('Count')
+    plt.title('No of Followers User have')
+    plt.savefig(r"static\FollowersAnaly.png")
+    plt.clf()
+    return
+
+
+def getNoFollowing(Analysis):
+    name2 = ['1-100','101-200','201-300','301-400','401-500','501-600','600+']
+    count2 = [0, 0,0,0,0,0,0]
+    for i in Analysis:
+        if i >=0 and i<=100:
+            count2[0] += 1
+        elif i >=101 and i<=200:
+            count2[1] += 1
+        elif i >=201 and i<=300:
+            count2[2] += 1
+        elif i >=301 and i<=400:
+            count2[3] += 1
+        elif i >=401 and i<=500:
+            count2[4] += 1
+        elif i >=501 and i<=600:
+            count2[5] += 1
+        else:
+            count2[6] += 1
+    plt.bar(name2, count2)
+    # plt.bar(name, count, align='center', alpha=0.5)
+    plt.ylabel('Count')
+    plt.title('No of Following User have')
+    plt.savefig(r"static\FollowingAnaly.png")
+    plt.clf()
+    return
+
+def getNoTweets(Analysis):
+    name2 = ['1-250', '215-500', '501-750', '751-1000', '1001-1250', '1251-1500', '1500+']
+    count2 = [0, 0, 0, 0, 0, 0, 0]
+    for i in Analysis:
+        if i >= 0 and i <= 250:
+            count2[0] += 1
+        elif i >= 251 and i <= 500:
+            count2[1] += 1
+        elif i >= 501 and i <= 750:
+            count2[2] += 1
+        elif i >= 751 and i <= 1000:
+            count2[3] += 1
+        elif i >= 1001 and i <= 1250:
+            count2[4] += 1
+        elif i >= 1251 and i <= 1500:
+            count2[5] += 1
+        else:
+            count2[6] += 1
+    plt.figure(figsize=(10,5))
+    plt.bar(name2, count2)
+    #plt.bar(name, count, align='center', alpha=0.5)
+    plt.ylabel('Count')
+    plt.title('No of tweets by particular user')
+    plt.savefig(r"static\NoOfTweetsAnaly.png")
+    plt.clf()
+    return
+
+
+
+
+
+
 
 
 
 
 
 def result(request):
+    no_of_tweets = 50
     s=request.GET['search']
     print(s)
     consumer_key='81vF03XpNUTi3TlKkskujt35D'
@@ -75,32 +183,55 @@ def result(request):
     auth=tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(access_token,access_token_secret)
     api=tweepy.API(auth)
-    public_tweets=tweepy.Cursor(api.search,q=s).items(100)
+    public_tweets=tweepy.Cursor(api.search,q=s).items(no_of_tweets)
     lsLocation = []
     lsTweets = []
     lsLang = []
-    laAnalysis = []
+    lsAge = []
+    lsAnalysis = []
+    lsVerified = []
+    lsFollower = []
+    lsFollowing = []
+    lsTweetsCount = []
     lsConti = set()
+    setLang = set()
+    setLoca = set()
     for tweet in public_tweets:
         #print(tweet.text)
         analysis=TextBlob(tweet.text)
         #print(analysis)
-        laAnalysis.append(analysis.sentiment.polarity)
+        lsAnalysis.append(analysis.sentiment.polarity)
         lsLocation.append(tweet.user.location)
+        setLoca.add(tweet.user.location)
         lsTweets.append(tweet.text)
+        lsVerified.append(tweet.user.verified)
+        lsFollower.append(tweet.user.followers_count)
+        lsFollowing.append(tweet.user.friends_count)
+        lsTweetsCount.append(tweet.user.statuses_count)
         lsConti.add(tweet.user.id)
-        lsLang.append(detect(tweet.text))
+        #lsAge.append(tweet.user.age)
+        tg = detect(tweet.text)
+        lsLang.append(tg)
+        setLang.add(tg)
         #print(tweet.user)
         #sdata = json.dumps(tweet.__json) 
         #print("###################################",tweet.user.location,"#####################################")
-    getAnalysis(laAnalysis)
+    getAnalysis(lsAnalysis)
     getLang(lsLang)
+    getVarified(lsVerified)
+    getNoFollowing(lsFollowing)
+    getCouontFollower(lsFollower)
+    getNoTweets(lsTweetsCount)
     #print(lsLang)
     Info = {}
     Info['location'] = lsLocation
-    Info['tweets'] = lsTweets
+    Info['no_of_tweets'] = no_of_tweets
+    #Info['tweets'] = lsTweets
     Info['languages'] = lsLang 
-    Info['Analysis'] = laAnalysis
+    Info['Analysis'] = lsAnalysis
     Info['Analysis_Img'] = 'Analysis.png'
     Info['Contribut'] = len(lsConti) 
-    return render(request,'result.html',{'result':Info})
+    Info['TotLang'] = len(setLang)
+    Info['TotLoca'] = len(setLoca) 
+    
+    return render(request,'result.html',{'tweets':lsTweets,'keyword':s,'result':Info})
